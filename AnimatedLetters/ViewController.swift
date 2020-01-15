a880a6fcf7d7247e48f3c731fec0452c53fd8cf0
Ron Herrema <rherrema@gmail.com>
2020-01-15 19:52:40 +0000

    Initial Commit

---
+++ AnimatedLetters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedLetters
+//
+//  Created by R J Herrema on 15/01/2020.
+//  Copyright © 2020 R J Herrema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
